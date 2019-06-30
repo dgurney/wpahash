16763010e71c89e63fa2a53b0c4976ad4c9772ce
Daniel Gurney <dgurney99@gmail.com>
2019-06-30 23:16:50 +0300

    Initial wpahash commit

---
+++ hash.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"runtime"
+
+	"golang.org/x/sys/windows/registry"
+)
+
+// Thanks Lucas for the initial C++ implementation and other information!
+func calculateEulaHash(wpaHive string, userBytes []byte) (uint64, error) {
+	// Debug symbols call the hash EulaHash
+	eulaHash := make([]byte, 0x80)
+	switch {
+	case len(userBytes) > 0:
+		// For testing
+		if len(userBytes) != 0x80 {
+			return 0, fmt.Errorf("the provided EulaHash is not 80 bytes long")
+		}
+		eulaHash = userBytes
+	default:
+		k, err := registry.OpenKey(registry.LOCAL_MACHINE, wpaHive+`\WPA\478C035F-04BC-48C7-B324-2462D786DAD7-5P-9`, registry.QUERY_VALUE)
+		if err != nil {
+			return 0, err
+		}
+		defer k.Close()
+		_, _, err = k.GetValue("", eulaHash)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	hashBytes := make([]byte, 8)
+	for i := 0; i < 16; i++ {
+		prev := 0x00
+		hashBytes[7] ^= eulaHash[i]
+		for j := 6; j >= 0; j-- {
+			prev += 0x10
+			hashBytes[j] ^= eulaHash[prev+i]
+		}
+	}
+	return binary.LittleEndian.Uint64(hashBytes), nil
+}
+
+func main() {
+	if runtime.GOOS != "windows" {
+		fmt.Printf("This program does not do anything meaningful on %s.\n", runtime.GOOS)
+		return
+	}
+
+	local := flag.Bool("l", false, "Calculate hash of the current installation. Doesn't work after Windows 10 1511 or before early Windows 8 builds.")
+	hive := flag.String("h", "", "Mounted hive name.")
+	flag.Parse()
+
+	if *local {
+		*hive = "SYSTEM"
+	}
+	if len(*hive) == 0 {
+		fmt.Println("The mounted hive name cannot be empty.\nUsage:")
+		flag.PrintDefaults()
+		return
+	}
+
+	hash, err := calculateEulaHash(*hive, []byte{})
+	if err != nil {
+		fmt.Println("Hash calculation error:", err)
+		return
+	}
+	fmt.Printf("%x\n", hash)
+}
